1d7d87906ad556a8f35fd00df19baca43630c62b
vieinfernale <weslleyskah@gmail.com>
2024-10-02 10:47:03 -0300

    ATCoder

--- ATCoder/abc373/B.py
+++ ATCoder/abc373/B.py
@@ -1,7 +1,21 @@
 def solution():
-    x = 0
-    return x
-    
+    distance = 0
+    d = {}
+    index = []
+    k = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    s = input()
+    for i in range(len(s)):
+        d[i] = s[i]
+    #for i in range(len(d)):
+    #    print(d[i], end=' ')
+    #print("\n")
+    for key, value in sorted(d.items(), key=lambda item: item[1]):
+        # print(f"{key} : {value}", end=' ')
+        index.append(key)
+    for i in range(len(index) - 1): 
+        distance += abs(index[i+1] - index[i])
+        
+    return distance
 
 def main():
     # tests = int(input())
