bd5ec8de9e9589a5e92657ac1558e1ff50a75fee
vieinfernale <weslleyskah@gmail.com>
2024-10-02 18:23:28 -0300

    ATCoder

--- ATCoder/abc373/C.py
+++ ATCoder/abc373/C.py
@@ -1,6 +1,31 @@
 def solution():
-    x = 0
-    return x
+    k = input()
+    s = input()
+    s2 = input()
+
+    L = []
+    L2 = []
+    
+    for x in s.split(" "):
+        L.append(int(x))
+    
+    for x in s2.split(" "):
+        L2.append(int(x))
+    
+    n = L[0]
+    n2 = L2[0]
+    
+    for i in range(len(L)):
+        if (L[i] > n):
+            n = L[i]
+    
+    for i in range(len(L2)):
+        if (L2[i] > n2):
+            n2 = L2[i]
+    
+    sum = n + n2
+    
+    return sum
 
 def main():
     result = solution()
