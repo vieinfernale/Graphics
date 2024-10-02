5fa171c1dd2b751e6548c5170b9c830f98bac7ed
vieinfernale <weslleyskah@gmail.com>
2024-10-02 18:40:07 -0300

    ATCoder

--- ATCoder/abc373/C.cpp
+++ ATCoder/abc373/C.cpp
@@ -1,9 +1,4 @@
 #include <bits/stdc++.h>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <array>
-#include <map>
 
 struct data
 {
@@ -12,66 +7,24 @@ struct data
 
 auto solution()
 {
-    std::string s, s2, k, k2;
-    std::vector<int> v, v2;
-    int a;
-    long long n, n2;
-    long long max;
+    int n;
+    int a = -1e9, b = -1e9; /* -1 * 10^-9 */
 
-    std::cin >> a;
-    std::cin.ignore();
-    std::getline(std::cin, s);
-    std::getline(std::cin, s2);
-    
-    for (int i = 0; i < s.size(); i++) {
-        k += s[i];
-        if (s[i+1] == s[s.size()]) {
-            k += s[i+1];
-            v.push_back(std::stoll(k));
-        }
-        if (s[i+1] == ' ') {
-            v.push_back(std::stoll(k));
-            k = "";
-            i++;
-        }
+    std::cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        int x;
+        std::cin >> x;
+        a = std::max(a, x);
     }
 
-    for (int i = 0; i < s2.size(); i++) {
-        k2 += s2[i];
-        if (s2[i+1] == s2[s2.size()]) {
-            k2 += s2[i+1];
-            v2.push_back(std::stoll(k2));
-        }
-        if (s2[i+1] == ' ') {
-            v2.push_back(std::stoll(k2));
-            k2 = "";
-            i++;
-        }
+    for (int i = 0; i < n; ++i) {
+        int x;
+        std::cin >> x;
+        b = std::max(b, x);
     }
 
-    n = v[0];
-    n2 = v2[0];
-
-    for (int i = 0; i < v.size(); i++){
-        /* std::cout << v[i] << " "; */
-        if (v[i] > n) {
-            n = v[i];
-        }
-
-    }
-
-    for (int i = 0; i < v2.size(); i++) {
-        /* std::cout << v2[i] << " "; */
-        if (v2[i] > n2) {
-            n2 = v2[i];
-        }
-    }
-
-    /* std::cout << n << " " << n2 << std::endl; */
-
-    max = n + n2;
-
-    return max;
+    return a + b;
 }
 
 int main()
