8889f1414f0e9a70a793b0b8c6a86c50ef8aaba7
vieinfernale <weslleyskah@gmail.com>
2024-10-02 11:53:55 -0300

    ATCoder

--- ATCoder/bones.py
+++ ATCoder/bones.py
@@ -1,14 +1,10 @@
 def solution():
     x = 0
     return x
-    
 
 def main():
-    # tests = int(input())
-    tests = 1
-    for t in range(tests):
-        result = solution()
-        print(result)
+    result = solution()
+    print(result)
 
 if __name__ == "__main__":
     main()
