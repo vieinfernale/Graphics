17a73f8eedb6e2143b64101e035d33784f5b9646
vieinfernale <weslleyskah@gmail.com>
2024-10-16 11:54:15 -0300

    C++

---
+++ Cpp/Include/Include.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "header.h"
+
+int main() {
+    std::cout << "#include <standard library> : used for including Standard Library headers." << std::endl;
+
+    std::cout << "std:: prefix is used in C++ to refer to functions, classes, and objects that belong to the Standard Library." << std::endl; 
+
+    std::cout << "#include \"header.h\" :  used for including Local header files in the same directory of the .cpp file." << std::endl;
+}
